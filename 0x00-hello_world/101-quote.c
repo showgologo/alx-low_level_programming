052818215b7261c7b76eb83c51cfc05ff71898de
showgologo <emmanueldprof@gmail.com>
2022-09-08 14:52:11 -0700

    low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,13 @@
 /**
  * main- Entry point
  *
- * Return: Always 0 (Succes)
+ * Return: Return 1
  */
 int main(void)
 {
 	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      s);
-	return (1);
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
+	return(1);
 }
