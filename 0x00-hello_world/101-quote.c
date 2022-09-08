5a44a033da951026c9626ba5115d48e07768df3f
showgologo <emmanueldprof@gmail.com>
2022-09-08 14:56:37 -0700

    low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,7 @@
 #include <string.h>
 #include <unistd.h>
 /**
- * main- Entry point
- *
+ * main - main func
  * Return: Return 1
  */
 int main(void)
@@ -11,6 +10,7 @@ int main(void)
 	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
-	return(1);
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      s);
+	return (1);
 }
