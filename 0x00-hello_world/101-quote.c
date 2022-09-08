cd4dbfde3ac29fd1591965e2763a6539923cf10e
showgologo <emmanueldprof@gmail.com>
2022-09-08 14:45:08 -0700

    low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <string.h>
+#include <unistd.h>
 /**
- * main- This is Entry point
- *
- * Return: Return 0
+ * main - main block
+ * Return: 1
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      s);
 	return (1);
 }
