7ba2c87b85880091efce025a67c0c897bcefea59
showgologo <emmanueldprof@gmail.com>
2022-09-08 14:43:32 -0700

    low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- This is Entry point
+ *
+ * Return: Return 0
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	return (1);
+}
