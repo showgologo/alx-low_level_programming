26206169e5c8a60dd9477f25978b1e1d37c71280
showgologo <emmanueldprof@gmail.com>
2022-09-08 13:38:28 -0700

    this is for low level programing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - this is the main block
+ * Return: Return 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
