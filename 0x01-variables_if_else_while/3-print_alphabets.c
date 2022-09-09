c7e7bf7227940cc96454e13368128aa6ac118037
showgologo <emmanueldprof@gmail.com>
2022-09-09 14:19:16 -0700

    variables if else while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 /**
  * main - main block
- * Description: Get a random number and print the number
- * and if it is positive, negative, or zero
+ * Description: Use `putchar` function to print the alphabet in lowercase.
  * Return: 0
  */
 int main(void)
 {
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+
+	i = 'A';
+
+	while (i <= 'Z')
+	{
+		putchar(i);
+		i++
+	}
+
+	putchar('\n');
+
+	return (0);
+}
