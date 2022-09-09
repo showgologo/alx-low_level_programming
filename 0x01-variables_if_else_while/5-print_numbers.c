fa1b3747c9dfd026a0eb4eac29dd2e4c2a4d4b86
showgologo <emmanueldprof@gmail.com>
2022-09-09 14:42:11 -0700

    variables if else while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,14 +4,15 @@
  * Description: print zero to nine
  * Return: 0
  */
-int main(void){
-
+int main(void)
+{
 	int a = 0;
 	
-	while(a < 10){
+	while(a < 10)
+	{
 		printf("%d", a);
 		a++;
-		printf("\n");
+		putchar("\n");
 	}
 
 	return 0;
