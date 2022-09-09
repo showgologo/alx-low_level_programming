a78db698e939067e4a4aed3b60eb15eb30b58e2d
showgologo <emmanueldprof@gmail.com>
2022-09-09 15:29:10 -0700

    variables if else while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,15 @@ int main(void)
 
 	while (a < 10)
 	{
-		putchar (a + '0');
-		if (a < 9);
+		putchar(a + '0');
+		if (a < 9)
 		{
-			putchar (44);
-			putchar (32);
+			putchar(44);
+			putchar(32);
 		}
 		a++;
 	}
 	putchar('\n');
+
+	return (0);
 }
