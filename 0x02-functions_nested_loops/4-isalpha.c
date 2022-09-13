64a685ec68a28ae4085d704c27eace20bde4e199
showgologo <emmanueldprof@gmail.com>
2022-09-13 15:09:29 -0700

    functions nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,13 @@
 #include "main.h"
 /**
- * main - this the main
- * Description: Functions Nested loops
- * Return: Program returns 0
- */
+ *  * _isalpha - Check if character is an alphabetic character.
+ *   * @c: type int character
+ *    * Return: 1 if letter, lowercase or uppercase, and 0 otherwise
+ *     */
+int _isalpha(int c)
+{
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+		return (1);
+	else
+		return (0);
+}
