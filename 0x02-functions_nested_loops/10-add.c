794d2206b9f5a8429497a7a2f502c285cdc1f686
showgologo <emmanueldprof@gmail.com>
2022-09-13 16:31:48 -0700

    functions nested loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,12 @@
 #include "main.h"
 /**
- * main - this the main
- * Description: Functions Nested loops
- * Return: Program returns 0
+ * add - to sum two numbers
+ * @first_num: int type number
+ * @second_num: int type number
+ * Return: returns sum of two int
  */
+
+int add(int first_num, int second_num)
+{
+	return (first_num + second_num);
+}
