687c83be96c977c753dbb2bb148df6d9f426bc84
showgologo <emmanueldprof@gmail.com>
2022-09-13 15:29:12 -0700

    functions nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,30 @@
 #include "main.h"
 /**
- * main - this the main
+ * print_sign - this the main
+ * @n: character type
  * Description: Functions Nested loops
  * Return: Program returns 0
  */
+
+int print_sign(int n);
+{
+	char n = 0; greater = '+'; lower = '-'; equal = '0';
+
+	if (n > 0)
+	{
+		_putchar(greater);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(lower);
+		return (-1);
+	}
+	else
+	{
+		_putchar(equal);
+		return (0);
+	}
+	_putchar('\n');
+
+}
