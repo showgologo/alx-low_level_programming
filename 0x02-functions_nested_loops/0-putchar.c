9446779fde324ea7e989e7f6785ad0ad64625e8c
showgologo <emmanueldprof@gmail.com>
2022-09-13 11:06:22 -0700

    Functions nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,3 +4,19 @@
  * Description: Functions Nested loops
  * Return: Program returns 0
  */
+
+int main(void)
+{
+	char my_char[] = "_putchar";
+	int loop = 0;
+
+	while (my_char[loop] != '\0')
+	{
+		_putchar(my_char[loop]);
+		loop++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
