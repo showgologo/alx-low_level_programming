b8c4409a134fdaf6c8df3ea92d8910b15076af44
showgologo <emmanueldprof@gmail.com>
2022-09-13 14:45:56 -0700

    functions nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,3 +4,21 @@
  * Description: Functions Nested loops
  * Return: Program returns 0
  */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char alphabet;
+
+	while (i < 10)
+	{
+		alphabet = 'a';
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
