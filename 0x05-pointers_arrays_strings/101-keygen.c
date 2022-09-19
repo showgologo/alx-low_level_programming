b1fce797c21f121db840375682cfc8306a177870
showgologo <emmanueldprof@gmail.com>
2022-09-19 13:47:34 -0700

    pointers array and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,42 +1,21 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-/**
- * _atoi - Convert a string to integer.
- * @s: char array string
- * Description: Number in the string can be preceded by an infinite
- * number of characters.
- * You need to take into account all -/+ signs before the number.
- * If there are no numbers in the string, return 0.
- * No need to check for overflow.
- * Not allowed to hard-code special values.
- * Return: first integer found in string
- */
-
-int _atoi(char *s)
+int main(void)
 {
-	int i;
-	int h, p;
+	int myrand;
+	int count;
+	int total;
 
-	h = 0;
-	p = -1;
-	for (i = 0; s[i] != '\0'; i++)
+	srand(time(NULL));
+	for (count = 0, total = 2772; total > 122; count++)
 	{
-		if (s[i] == '-')
-			p *= -1;
-
-		if (s[i] > 47 && s[i] < 58)
-		{
-			if (h < 0)
-				h = (h * 10) - (s[i] - '0');
-			else
-				h = (s[i] - '0') * -1;
-
-			if (s[i + 1] < 48 || s[i + 1] > 57)
-				break;
-		}
+		myrand = (rand() % 125) + 1;
+		printf("%c", myrand);
+		total -= myrand;
 	}
-	if (p < 0)
-		h *= -1;
+	printf("%c", total);
 
-	return (h);
+	return (0);
 }
