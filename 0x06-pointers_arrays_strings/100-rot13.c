f3e097a15222ca1c6d21f843bf0c720ae3492e74
showgologo <emmanueldprof@gmail.com>
2022-09-22 09:27:09 -0700

    This is more pointers arrays strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,25 +7,38 @@
  */
 char *rot13(char *str)
 {
-	int i = 0;
+	int indx1 = 0, indx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+		'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'a', 'b', 'c', 'd',
+		'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p',
+		'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E',
+		'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'a', 'b', 'c',
+		'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm'};
 
-	while (str[i] != '\0')
+	while (str[indx1])
 	{
-		while ((str[i] >= 'a' && str[i] <= 'z') ||
-		(str[i] >= 'A' && str[i] <= 'Z'))
+		for (indx2 = 0; indx2 < 52; indx2++)
 		{
-			if ((str[i] >= 'a' && str[i] <= 'm')
-			|| (str[i] >= 'A' && str[i] <= 'M'))
+			if (str[indx1] == alphabet[indx2])
 			{
-				str[i] = str[i] + 13;
+				str[indx1] = rot13key[indx2];
+				break;
 			}
-			else
-			{
-				str[i] = str[i] - 13;
-			}
-			i++;
 		}
-		i++;
+		indx1++;
 	}
+
 	return (str);
 }
